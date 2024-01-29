81b41b118ad9d3aa4e02ee5b71675390ca662d54
Farbod Kaymasi <Farbodkimasy@gmail.com>
2024-01-29 13:08:39 +0330

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,84 @@
+import React, { useState } from 'react';
+import './App.css';
+
+export default function App() {
+	const questions = [
+		{
+			questionText: 'Where is the capital of France?',
+			answerOptions: [
+				{ answerText: 'New York', isCorrect: false },
+				{ answerText: 'London', isCorrect: false },
+				{ answerText: 'Paris', isCorrect: true },
+				{ answerText: 'Moscow', isCorrect: false },
+			],
+		},
+		{
+			questionText: 'Who is the CEO of Tesla?',
+			answerOptions: [
+				{ answerText: 'Jeff Bezos', isCorrect: false },
+				{ answerText: 'Elon Musk', isCorrect: true },
+				{ answerText: 'Bill Gates', isCorrect: false },
+				{ answerText: 'Tony Stark', isCorrect: false },
+			],
+		},
+		{
+			questionText: 'Which company manufactures the iPhone?',
+			answerOptions: [
+				{ answerText: 'Apple', isCorrect: true },
+				{ answerText: 'Intel', isCorrect: false },
+				{ answerText: 'Amazon', isCorrect: false },
+				{ answerText: 'Microsoft', isCorrect: false },
+			],
+		},
+		{
+			questionText: 'How many Harry Potter books have been written so far?',
+			answerOptions: [
+				{ answerText: '1', isCorrect: false },
+				{ answerText: '4', isCorrect: false },
+				{ answerText: '6', isCorrect: false },
+				{ answerText: '7', isCorrect: true },
+			],
+		},
+	];
+
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [showScore, setShowScore] = useState(false);
+  const [score, setScore] = useState(0);
+
+  const handleAnswerOptionClick = (isCorrect) => {
+		if (isCorrect) {
+			setScore(score + 1);
+		}
+
+		const nextQuestion = currentQuestion + 1;
+		if (nextQuestion < questions.length) {
+			setCurrentQuestion(nextQuestion);
+		} else {
+			setShowScore(true);
+		}
+	};
+
+	return (
+		<div className='app'>
+			{showScore ? (
+				<div className='score-section'>
+					Your score is {score} out of {questions.length} questions.
+				</div>
+			) : (
+				<>
+					<div className='question-section'>
+						<div className='question-count'>
+							<span className='font-bold'>Question {currentQuestion + 1}</span>/{questions.length}
+						</div>
+						<div className='question-text text-center'>{questions[currentQuestion].questionText}</div>
+					</div>
+					<div className='answer-section ml-10' style={{ textAlign: 'center' }}>
+						{questions[currentQuestion].answerOptions.map((answerOption) => (
+							<button onClick={() => handleAnswerOptionClick(answerOption.isCorrect)}>{answerOption.answerText}</button>
+						))}
+					</div>
+				</>
+			)}
+		</div>
+	);
+}
